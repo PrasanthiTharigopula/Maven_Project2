0792832d949df8cb6013b361e1c270b35de88cd8
Prasanthi <prasanthi.tharigopula@gmail.com>
2022-08-24 14:41:11 -0400

    sample Project

---
+++ src/main/java/testgroup/internal/SampleProviderImpl.java
@@ -0,0 +1,15 @@
+/**
+ * iPOJO sample service provider.
+ * REMOVE THIS CLASS AS SOON AS YOU UNDERSTOOD WHAT THIS FILE GOING TO DO.
+ */
+package testgroup.internal;
+
+import testgroup.SampleIntf;
+
+public class SampleProviderImpl implements SampleIntf {
+
+	public String sayHello() {
+		return "Hello";
+	}
+
+}
